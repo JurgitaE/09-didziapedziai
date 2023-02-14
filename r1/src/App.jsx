8b98663dab6c2f60f0b964806dda1857fa68f8f0
Jurgita <jurgita.eicinaite@yahoo.com>
2023-02-14 09:17:59 +0200

    008

--- r1/src/App.jsx
+++ r1/src/App.jsx
@@ -1,21 +1,22 @@
-import './App.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.scss';
 import AnimalLi from './Components/008/AnimalLi';
+import Click from './Components/008/Click';
 
 const data = [
-    {id: 1, animal: 'Racoon', color: 'crimson', bold: true},
-    {id: 4, animal: 'Fox', color: 'brown', bold: true},
-    {id: 17, animal: 'Beaver', color: 'skyblue', bold: false},
-    {id: 3, animal: 'Unicorn', color: 'coral', bold: true},
+    { id: 1, animal: 'Racoon', color: 'crimson', bold: true },
+    { id: 4, animal: 'Fox', color: 'brown', bold: true },
+    { id: 17, animal: 'Beaver', color: 'skyblue', bold: false },
+    { id: 3, animal: 'Unicorn', color: 'coral', bold: true },
 ];
 
 function App() {
-   
     return (
         <div className="App">
             <header className="App-header">
+                <Click />
 
-                 <div className="card">
+                {/* <div className="card">
                     <div className="card-header">
                         <h2>Animal List</h2>
                     </div>
@@ -24,8 +25,7 @@ function App() {
                             data.map((a, i)=> <AnimalLi key={a.id} animal={a} index={i}/>)
                         }
                     </ul>
-                </div>
-
+                </div> */}
             </header>
         </div>
     );
